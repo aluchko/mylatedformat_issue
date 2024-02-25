dd34223fd037df897d487a6e0be1e44304fede85
aluchko <luchko@rogue7.ca>
2024-02-25 14:03:49 -0700

    readd readme

--- cleanup.sh
+++ cleanup.sh
@@ -2,5 +2,5 @@
 # Find and remove all *.log, *.aux, and *pdf files
 find . -name "*.log" -type f -delete
 find . -name "*.aux" -type f -delete
-find . -name "*.pdf" -type f -delete
+find . -depth -path "./mylatexformat/*" -prune -o -name "*.pdf" -type f -delete
 find . -name "*.fmt" -type f -delete
